dbb54be43f865b4fccbfad42907974b041ac1e53
tmaioli <tmaioli@deloitte.it>
2024-01-25 17:37:46 +0100

    fix: removed unused postRouter

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,11 +1,9 @@
 import { createTRPCRouter } from '../trpc'
-import { postsRouter } from './posts'
 import { packsRouter } from './packs'
 // import { createTRPCRouter } from "@/server/api/trpc";
 // import { s3Router } from "./routers/s3";
 
 export const appRouter = createTRPCRouter({
-  posts: postsRouter,
   packs: packsRouter,
   // s3: s3Router
 })
