2d1161fc4c713ee53bcf8ac39637b02e91efeb5e
tmaioli <tmaioli@deloitte.it>
2024-02-15 12:43:26 +0100

    feat: addition of footer in PageLayout

--- packages/ui/src/PageLayout.tsx
+++ packages/ui/src/PageLayout.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, YStack } from 'tamagui'
+import { Anchor, Paragraph, ScrollView, Separator, YStack } from 'tamagui'
 import { Header } from 'app/components/header'
 import { LayoutChangeEvent, Platform } from 'react-native'
 
@@ -11,40 +11,52 @@ export const PageLayout = ({
   scrollViewProps?: object
   layout?: React.RefObject<LayoutChangeEvent['nativeEvent']['layout'] | null>
 }) => {
+  const Footer = () => (
+    <Paragraph textAlign="right" px={'$4'} fontSize={'$4'}>
+      Built with ❤️ by <Anchor href="https://tomashco/github.io">Tom</Anchor>
+    </Paragraph>
+  )
   return (
-    <ScrollView backgroundColor={'$color4'} {...scrollViewProps}>
-      {Platform.OS === 'web' && (
+    <YStack f={1} backgroundColor={'$color4'}>
+      <ScrollView mb={Platform.OS !== 'web' ? '$13' : 0} {...scrollViewProps}>
+        {Platform.OS === 'web' && (
+          <YStack
+            p={'$4'}
+            pb={0}
+            f={1}
+            jc="center"
+            w={'100%'}
+            ai="center"
+            alignSelf="center"
+            $gtMd={{ width: '45rem' }}
+            // space
+          >
+            <Header />
+          </YStack>
+        )}
         <YStack
           p={'$4'}
-          pb={0}
           f={1}
           jc="center"
           w={'100%'}
-          ai="center"
           alignSelf="center"
-          $gtMd={{ width: '45rem' }}
+          ai="center"
+          $gtSm={{ width: '35rem' }}
           space
+          onLayout={(event) => {
+            const { x, y, height, width } = event.nativeEvent.layout
+            if (layout?.current) Object.assign(layout.current, { x, y, height, width })
+          }}
         >
-          <Header />
+          {children}
+        </YStack>
+        {Platform.OS !== 'web' && <Footer />}
+      </ScrollView>
+      {Platform.OS === 'web' && (
+        <YStack position="absolute" bottom={0} right={0}>
+          <Footer />
         </YStack>
       )}
-      <YStack
-        mb={'$13'}
-        p={'$4'}
-        f={1}
-        jc="center"
-        w={'100%'}
-        alignSelf="center"
-        ai="center"
-        $gtSm={{ width: '35rem' }}
-        space
-        onLayout={(event) => {
-          const { x, y, height, width } = event.nativeEvent.layout
-          if (layout?.current) Object.assign(layout.current, { x, y, height, width })
-        }}
-      >
-        {children}
-      </YStack>
-    </ScrollView>
+    </YStack>
   )
 }
